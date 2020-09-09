cb8862230c0bb77a48c102fec43981d92b65727a
xiaobin.lin <xiaobin.lin@qlchat.com>
2020-09-09 15:06:48 +0800

    随机数

--- naxon-common-tool-component/src/main/java/com/naxon/tool/common/NaxonUtils.java
+++ naxon-common-tool-component/src/main/java/com/naxon/tool/common/NaxonUtils.java
@@ -1,5 +1,7 @@
 package com.naxon.tool.common;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * @author naxon
  * 通用工具类
@@ -27,6 +29,25 @@ public class NaxonUtils {
 
     public static Boolean isNotEmpty(Object obj) {
         return !isEmpty(obj);
+    }
+
+    public static String randomNumber() {
+        return randomNumber(6);
+    }
+
+    public static String randomNumber(Integer count) {
+        if (count == null) {
+            count = 6;
+        }
+        if (count < 1 || count > 18) {
+            throw new IllegalArgumentException("期待参数取值：1-18，实际取值：" + count);
+        }
+
+        Double start = Math.pow(10, count);
+        Double end = Math.pow(10, count + 1);
+
+        long random = ThreadLocalRandom.current().nextLong(start.longValue(), end.longValue());
+        return StringUtils.getString(random);
     }
 
 }
