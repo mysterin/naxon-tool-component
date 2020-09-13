6ba50ab376863036f6997f34dce81c40ff0e951b
naxon <mysterin@163.com>
2020-09-13 22:48:10 +0800

    okhttp

---
+++ naxon-http-tool-component/src/main/java/com/naxon/tool/http/OkHttpClientFactory.java
@@ -0,0 +1,15 @@
+package com.naxon.tool.http;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * http 客户端工厂
+ */
+public class OkHttpClientFactory {
+
+    public OkHttpClient okHttpClient;
+
+    public OkHttpClient getClient() {
+        return null;
+    }
+}
