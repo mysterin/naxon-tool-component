708433e5f1085533efecc95e161179c4383fee59
xiaobin.lin <xiaobin.lin@qlchat.com>
2020-09-24 21:53:12 +0800

    urlencode 类型

--- naxon-http-tool-component/src/main/java/com/naxon/tool/http/MediaType.java
+++ naxon-http-tool-component/src/main/java/com/naxon/tool/http/MediaType.java
@@ -2,4 +2,5 @@ package com.naxon.tool.http;
 
 public class MediaType {
     public static final okhttp3.MediaType APPLICATION_JSON = okhttp3.MediaType.parse("application/json; charset=utf-8");
+    public static final okhttp3.MediaType FORM_URLENCODED = okhttp3.MediaType.parse("application/x-www-form-urlencoded;charset=utf-8");
 }
